84f2d89b3f1b7f08f801a2f0918529ca99b92d08
Marcin Kotas <markotny97@gmail.com>
2020-03-25 21:47:31 +0100

    Configure eslint and prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+/* eslint-disable @typescript-eslint/camelcase */
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {StoreModule} from '@ngrx/store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreRouterConnectingModule} from '@ngrx/router-store';
+import {NgOidcClientModule} from 'ng-oidc-client';
+import {environment} from '../environments/environment';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production
+    }),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot(),
+    NgOidcClientModule.forRoot({
+      oidc_config: {
+        client_id: 'ng-oidc-client-identity',
+        response_type: 'id_token token',
+        scope: 'openid profile offline_access api1',
+        authority: 'https://ng-oidc-client-server.azurewebsites.net',
+        redirect_uri: 'http://localhost:4200/callback.html',
+        post_logout_redirect_uri: 'http://localhost:4200/signout-callback.html',
+        silent_redirect_uri: 'http://localhost:4200/renew-callback.html',
+        automaticSilentRenew: true
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
