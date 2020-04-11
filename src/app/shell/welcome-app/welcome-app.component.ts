9a22137931adad003d474c965b94e8582f479c5c
Bartosz Prokopowicz <prokopowicz@vialutions.com>
2020-04-11 21:36:34 +0200

    Created header

--- src/app/shell/welcome-app/welcome-app.component.ts
+++ src/app/shell/welcome-app/welcome-app.component.ts
@@ -1,12 +1,19 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Inject, OnDestroy} from '@angular/core';
+import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'app-welcome-app',
   templateUrl: './welcome-app.component.html',
   styleUrls: ['./welcome-app.component.scss']
 })
-export class WelcomeAppComponent implements OnInit {
-  constructor() {}
+export class WelcomeAppComponent implements OnInit, OnDestroy {
+  constructor(@Inject(DOCUMENT) private document) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.document.body.classList.add('background-light');
+  }
+
+  ngOnDestroy(): void {
+    this.document.body.classList.remove('background-light');
+  }
 }
