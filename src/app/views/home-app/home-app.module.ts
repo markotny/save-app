afdda731d14970887576f1d774130a09a6458200
Bartosz Prokopowicz <prokopowicz@vialutions.com>
2020-04-12 16:30:12 +0200

    Added footer

--- src/app/views/home-app/home-app.module.ts
+++ src/app/views/home-app/home-app.module.ts
@@ -4,9 +4,10 @@ import {CommonModule} from '@angular/common';
 import {HomeAppComponent, HomeHeaderComponent} from './index';
 import { HomeMainSentenceComponent } from './home-main-sentence/home-main-sentence.component';
 import { HomeFeaturesComponent } from './home-features/home-features.component';
+import { HomeFooterComponent } from './home-footer/home-footer.component';
 
 @NgModule({
-  declarations: [HomeAppComponent, HomeHeaderComponent, HomeMainSentenceComponent, HomeFeaturesComponent],
+  declarations: [HomeAppComponent, HomeHeaderComponent, HomeMainSentenceComponent, HomeFeaturesComponent, HomeFooterComponent],
   imports: [CommonModule]
 })
 export class HomeAppModule {}
