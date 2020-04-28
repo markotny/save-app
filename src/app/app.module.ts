59367ae41f84369f15e54bcea7565c777063eeaa
Marcin Kotas <markotny97@gmail.com>
2020-04-29 01:01:01 +0200

    add missing toast module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import {NgModule} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {BrowserModule} from '@angular/platform-browser';
-
 import {ShellModule} from '@shell/shell.module';
-
 import {CoreModule} from './core/core.module';
+import {ToastModule} from 'primeng/toast';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, ShellModule, CoreModule, AppRoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule, CoreModule, ShellModule, ToastModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
