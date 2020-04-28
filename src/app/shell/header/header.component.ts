d6fa6d83e4eb75e99e12ad857ce7abc08c78212c
Marcin Kotas <markotny97@gmail.com>
2020-04-29 00:56:16 +0200

    create sidebar and header animations

--- src/app/shell/header/header.component.ts
+++ src/app/shell/header/header.component.ts
@@ -1,23 +1,59 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output, OnDestroy} from '@angular/core';
 import {accountMenu} from '@shell/account-menu.model';
 import {OidcFacade} from 'ng-oidc-client';
 import {MenuItem} from 'primeng/api/menuitem';
-import {Observable} from 'rxjs';
-import {map, share} from 'rxjs/operators';
+import {Observable, Subscription} from 'rxjs';
+import {map, share, filter, pairwise} from 'rxjs/operators';
+import {trigger, state, style, animate, transition} from '@angular/animations';
+import {MediaObserver, MediaChange} from '@angular/flex-layout';
 
 @Component({
   selector: 'app-header',
+  animations: [
+    trigger('openClose', [
+      state(
+        'true',
+        style({
+          transform: 'rotate(90deg)'
+        })
+      ),
+      state(
+        'false',
+        style({
+          transform: 'rotate(0deg)'
+        })
+      ),
+      transition('true <=> false', [animate('0.1s ease-in-out')])
+    ]),
+    trigger('headerFade', [
+      state(
+        'true',
+        style({
+          opacity: 1
+        })
+      ),
+      state(
+        'false',
+        style({
+          opacity: 0
+        })
+      ),
+      transition('false => true', [animate('0.1s 0.4s ease-in')]),
+      transition('true => false', [animate('0.1s ease-out')])
+    ])
+  ],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   @Output() sidenavToggle = new EventEmitter();
 
+  accountMenuOpen = false;
   accountMenuItems: MenuItem[];
   userName$: Observable<string>;
-  headerStyle: unknown;
+  watcher$: Subscription;
 
-  constructor(private oidcFacade: OidcFacade) {}
+  constructor(private oidcFacade: OidcFacade, public mediaObserver: MediaObserver) {}
 
   ngOnInit() {
     this.userName$ = this.oidcFacade.identity$.pipe(
@@ -25,9 +61,25 @@ export class HeaderComponent implements OnInit {
       share()
     );
     this.accountMenuItems = accountMenu(() => this.oidcFacade.signoutRedirect());
+    this.initWatcher();
+  }
+
+  private initWatcher(): void {
+    this.watcher$ = this.mediaObserver
+      .asObservable()
+      .pipe(
+        map((changes: MediaChange[]) => changes[0]),
+        pairwise(),
+        filter(([prev]) => prev.mqAlias === 'xs')
+      )
+      .subscribe(() => this.sidenavToggle.emit(false));
   }
 
   onToggleSidenav(): void {
     this.sidenavToggle.emit(true);
+  }
+
+  ngOnDestroy(): void {
+    this.watcher$.unsubscribe();
   }
 }
--- src/app/shell/main-app/main-app.component.ts
+++ src/app/shell/main-app/main-app.component.ts
@@ -1,8 +1,62 @@
 import {Component, OnInit, OnDestroy, Inject} from '@angular/core';
 import {DOCUMENT} from '@angular/common';
+import {trigger, state, style, transition, animate, group, query, animateChild} from '@angular/animations';
+import {MediaObserver} from '@angular/flex-layout';
 
 @Component({
   selector: 'app-main-app',
+  animations: [
+    trigger('sidenavHideXs', [
+      state(
+        'true',
+        style({
+          transform: 'translateX(-100%)',
+          flex: '0 1 0'
+        })
+      ),
+      state(
+        'false',
+        style({
+          transform: 'translateX(0)',
+          flex: '1 0 250px'
+        })
+      ),
+      transition('false => true', [animate('0.5s ease-in')]),
+      transition('true => false', [animate('0.5s ease-out')])
+    ]),
+    trigger('sidenavMargin', [
+      state(
+        'true',
+        style({
+          marginLeft: 0
+        })
+      ),
+      state(
+        'false',
+        style({
+          marginLeft: '250px'
+        })
+      ),
+      transition('false => true', [group([query('@headerHeight', animateChild()), animate('0.5s ease-in')])]),
+      transition('true => false', [group([query('@headerHeight', animateChild()), animate('0.5s ease-out')])])
+    ]),
+    trigger('headerHeight', [
+      state(
+        'true',
+        style({
+          height: '50px'
+        })
+      ),
+      state(
+        'false',
+        style({
+          height: '86px'
+        })
+      ),
+      transition('false => true', [group([query('@headerFade', animateChild()), animate('0.2s ease-in')])]),
+      transition('true => false', [group([query('@headerFade', animateChild()), animate('0.2s 0.3s ease-out')])])
+    ])
+  ],
   templateUrl: './main-app.component.html',
   styleUrls: ['./main-app.component.scss']
 })
@@ -10,7 +64,7 @@ export class MainAppComponent implements OnInit, OnDestroy {
   sidebarVisible: boolean;
   rowStyle: unknown;
 
-  constructor(@Inject(DOCUMENT) private document) {}
+  constructor(@Inject(DOCUMENT) private document, public mediaObserver: MediaObserver) {}
   ngOnInit() {
     this.injectBackgroundColor();
   }
