fa6f5e0b642b19d3d9eb7edb373cde312255cef0
Bartosz Prokopowicz <prokopowicz@vialutions.com>
2020-05-02 15:16:50 +0200

    Changed env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   test: false,
   appName: 'wydatex',
-  authServerUri: 'https://localhost:5001',
-  thisUri: 'http://localhost:4200'
+  authServerUri: 'https://wydatex-identity.azurewebsites.net',
+  apiServerUri: 'https://wydatex-resource.azurewebsites.net/api/v1',
+  thisUri: 'https://wydatex.github.io'
 };
