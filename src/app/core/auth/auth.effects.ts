b5f8420b1522c610940c1c3cd7baf962fabf2ce8
Marcin Kotas <markotny97@gmail.com>
2020-05-06 15:59:11 +0200

    refactor auth

--- src/app/core/auth/auth.effects.ts
+++ src/app/core/auth/auth.effects.ts
@@ -1,12 +1,11 @@
-import {actionOidcRegister, actionOidcRegisterSuccess, actionApiUserAddUserSuccess, actionApiUserAddUserFail} from './auth.actions';
+import * as AuthActions from './auth.actions';
 import {Injectable} from '@angular/core';
 import {Router} from '@angular/router';
 import {createEffect, ofType, Actions} from '@ngrx/effects';
-import {tap, switchMap, concatMap, catchError, map, withLatestFrom} from 'rxjs/operators';
+import {tap, catchError, withLatestFrom, switchMapTo, mapTo, switchMap} from 'rxjs/operators';
 import {OidcActions, OidcFacade} from 'ng-oidc-client';
-import {environment} from '@env/environment';
-import {of} from 'rxjs';
-import {UserService} from '@services/user.service';
+import {of, from} from 'rxjs';
+import {AuthService} from '@core/auth/auth.service';
 import {MessageService} from 'primeng/api';
 import {LocalStorageService} from '@core/local-storage/local-storage.service';
 import {Store, select} from '@ngrx/store';
@@ -22,12 +21,86 @@ export class AuthEffects {
     private store: Store<OidcState>,
     private router: Router,
     private oidcFacade: OidcFacade,
-    private userService: UserService,
+    private authService: AuthService,
     private messageService: MessageService,
     private localStorageService: LocalStorageService
   ) {}
 
-  persistUser$$ = createEffect(
+  login$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AuthActions.login),
+        tap(() => this.oidcFacade.signinRedirect())
+      ),
+    {dispatch: false}
+  );
+
+  loadUser$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AuthActions.loginSuccess),
+      switchMap(() =>
+        from(this.oidcFacade.getUserManager().signinRedirectCallback()).pipe(
+          mapTo(AuthActions.ensureRegistered()),
+          catchError(error => of(OidcActions.OidcError({payload: error.message})))
+        )
+      )
+    )
+  );
+
+  register$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AuthActions.register),
+        tap(() => this.authService.register())
+      ),
+    {dispatch: false}
+  );
+
+  registerSuccess$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AuthActions.registerSuccess),
+      switchMap(() =>
+        from(this.oidcFacade.getUserManager().signinSilent()).pipe(
+          mapTo(AuthActions.ensureRegistered()),
+          catchError(error =>
+            error.message === 'login_required' ? of(AuthActions.login()) : of(OidcActions.OidcError({payload: error.message}))
+          )
+        )
+      )
+    )
+  );
+
+  ensureRegistered$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AuthActions.ensureRegistered),
+      switchMapTo(
+        this.authService.ensureRegistered().pipe(
+          mapTo(AuthActions.ensureRegisteredSuccess()),
+          catchError((error: Error) => of(AuthActions.ensureRegisteredFailure({payload: error.message})))
+        )
+      )
+    )
+  );
+
+  logoutOnFailedRegisteredCheck$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AuthActions.ensureRegisteredFailure),
+        tap(() => {
+          this.messageService.add({
+            severity: 'warn',
+            summary: 'User registration failed',
+            detail: 'You will be automatically signed out in 5 seconds.',
+            closable: false,
+            sticky: true
+          });
+          setTimeout(() => this.oidcFacade.signoutRedirect(), 5000);
+        })
+      ),
+    {dispatch: false}
+  );
+
+  cacheUser$ = createEffect(
     () =>
       this.actions$.pipe(
         ofType(OidcActions.UserDoneLoading),
@@ -37,30 +110,34 @@ export class AuthEffects {
     {dispatch: false}
   );
 
-  userSigningOut$ = createEffect(
+  clearCachedUser$ = createEffect(
     () =>
       this.actions$.pipe(
-        ofType(OidcActions.SignOutRedirect),
-        tap(() => {
-          this.localStorageService.clearState();
-        })
+        ofType(OidcActions.SignOutRedirect, OidcActions.OnUserSignedOut),
+        tap(() => this.localStorageService.clearState())
       ),
     {dispatch: false}
   );
 
-  userSignedOut$ = createEffect(
+  redirectToApp$ = createEffect(
     () =>
       this.actions$.pipe(
-        ofType(OidcActions.OnUserSignedOut),
-        tap(() => {
-          this.localStorageService.clearState();
-          this.router.navigate(['/']);
-        })
+        ofType(AuthActions.ensureRegistered),
+        tap(() => this.router.navigate(['app']))
       ),
     {dispatch: false}
   );
 
-  oidcDisplayError$ = createEffect(
+  redirectToHome$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(OidcActions.OnUserSignedOut, OidcActions.OidcError),
+        tap(() => this.router.navigate(['/']))
+      ),
+    {dispatch: false}
+  );
+
+  displayOidcError$ = createEffect(
     () =>
       this.actions$.pipe(
         ofType(OidcActions.SignInError, OidcActions.OidcError),
@@ -73,31 +150,5 @@ export class AuthEffects {
         )
       ),
     {dispatch: false}
-  );
-
-  register$ = createEffect(
-    () =>
-      this.actions$.pipe(
-        ofType(actionOidcRegister),
-        tap(() => {
-          const returnUrl = `${environment.thisUri}/register-callback`;
-          window.location.href = `${environment.authServerUri}/Identity/Account/Register?ReturnUrl=${returnUrl}`;
-        })
-      ),
-    {dispatch: false}
-  );
-
-  registerSuccess$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(actionOidcRegisterSuccess),
-      switchMap(() => this.oidcFacade.getUserManager().signinSilent()),
-      tap(() => this.router.navigate(['app'])),
-      concatMap(() =>
-        this.userService.addNewUser().pipe(
-          map(() => actionApiUserAddUserSuccess()),
-          catchError(error => of(actionApiUserAddUserFail({error})))
-        )
-      )
-    )
   );
 }
