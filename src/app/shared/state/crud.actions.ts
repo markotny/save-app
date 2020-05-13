ed0a57ba945c1bdcc5d33eed649d67962fe9c042
Marcin Kotas <markotny97@gmail.com>
2020-05-13 23:42:50 +0200

    remove unnecessary Entity type from crudActions

--- src/app/shared/state/crud.actions.ts
+++ src/app/shared/state/crud.actions.ts
@@ -1,17 +1,16 @@
 import {createAction, props} from '@ngrx/store';
 import {ModelBase} from '@wydatex/models';
-import {randomId, Id, Unsaved} from '@shared/types';
+import {randomId, Id} from '@shared/types';
 import {ApiModule} from './api-module.enum';
 
 export const crudActions = <DTO extends Partial<VM>, VM extends ModelBase, VMExtended extends VM = VM>(module: ApiModule) => {
   const name = ApiModule[module];
-  type Entity = VM & Unsaved;
   return {
     load: createAction(`[${name}] Load`),
-    getDetails: createAction(`[${name}] Get details`, props<{id: Id<Entity>}>()),
+    getDetails: createAction(`[${name}] Get details`, props<{id: Id<VM>}>()),
     add: createAction(`[${name}] Add`, (item: DTO) => ({tempId: randomId(), item})),
-    edit: createAction(`[${name}] Edit`, props<{id: Id<Entity>; item: DTO}>()),
-    remove: createAction(`[${name}] Remove`, props<{id: Id<Entity>}>()),
+    edit: createAction(`[${name}] Edit`, props<{id: Id<VM>; item: DTO}>()),
+    remove: createAction(`[${name}] Remove`, props<{id: Id<VM>}>()),
 
     loadSuccess: createAction(`[${name}/API] Load success`, props<{items: VM[]}>()),
     loadFailure: createAction(`[${name}/API] Load failure`, props<{error: Error}>()),
@@ -19,13 +18,13 @@ export const crudActions = <DTO extends Partial<VM>, VM extends ModelBase, VMExt
     getDetailsSuccess: createAction(`[${name}/API] Get details success`, props<{item: VMExtended}>()),
     getDetailsFailure: createAction(`[${name}/API] Get details failure`, props<{error: Error}>()),
 
-    addSuccess: createAction(`[${name}/API] Add success`, props<{tempId: Id<Entity>; item: VMExtended}>()),
-    addFailure: createAction(`[${name}/API] Add failure`, props<{tempId: Id<Entity>; error: Error}>()),
+    addSuccess: createAction(`[${name}/API] Add success`, props<{tempId: Id<VM>; item: VMExtended}>()),
+    addFailure: createAction(`[${name}/API] Add failure`, props<{tempId: Id<VM>; error: Error}>()),
 
     editSuccess: createAction(`[${name}/API] Edit success`, props<{item: VMExtended}>()),
-    editFailure: createAction(`[${name}/API] Edit failure`, props<{id: Id<Entity>; error: Error}>()),
+    editFailure: createAction(`[${name}/API] Edit failure`, props<{id: Id<VM>; error: Error}>()),
 
-    removeSuccess: createAction(`[${name}/API] Remove success`, props<{id: Id<Entity>}>()),
-    removeFailure: createAction(`[${name}/API] Remove failure`, props<{id: Id<Entity>; error: Error}>())
+    removeSuccess: createAction(`[${name}/API] Remove success`, props<{id: Id<VM>}>()),
+    removeFailure: createAction(`[${name}/API] Remove failure`, props<{id: Id<VM>; error: Error}>())
   };
 };
