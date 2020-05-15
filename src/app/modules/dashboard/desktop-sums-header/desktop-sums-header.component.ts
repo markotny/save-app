16ce84f39f6194f50d4b134fc8107d91a016f16e
Bartosz Prokopowicz <prokopowicz@vialutions.com>
2020-05-15 02:36:44 +0200

    Finished header for dashboard component

---
+++ src/app/modules/dashboard/desktop-sums-header/desktop-sums-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-desktop-sums-header',
+  templateUrl: './desktop-sums-header.component.html',
+  styleUrls: ['./desktop-sums-header.component.scss']
+})
+export class DesktopSumsHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
